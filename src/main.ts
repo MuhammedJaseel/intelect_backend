3cf57763c29f248f37af6ff5d69b9503d3504b4c
MuhammedJaseel <jaseelmanamulli@gmail.com>
2025-07-04 21:28:12 +0400

    Api Added 0.0.41

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
+
+// ✅ Export the Express server for Vercel
+export default server;
