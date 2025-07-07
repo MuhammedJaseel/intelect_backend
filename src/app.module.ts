a4b50e798ab6a4674d80b0d8b8e359c8abe487b5
MuhammedJaseel <jaseelmanamulli@gmail.com>
2025-07-07 19:48:56 +0400

    OP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { Inquiry, InquirySchema } from './app.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Inquiry, InquirySchema } from './app.schema';
 
 @Module({
-  // imports: [
-  //   MongooseModule.forRoot(
-  //     process.env.MONGO_URI || 'mongodb://localhost:27017/inquiries',
-  //   ),
-  //   MongooseModule.forFeature([{ name: Inquiry.name, schema: InquirySchema }]),
-  // ],
-  // controllers: [AppController],
-  // providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/inquiries',
+    ),
+    MongooseModule.forFeature([{ name: Inquiry.name, schema: InquirySchema }]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
